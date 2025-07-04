fbc5c67e67acd48353b5307ff0ffb51caf2a4b3d
Saksham Arora <80617896+SakshamAror@users.noreply.github.com>
2025-07-04 13:06:21 +0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import 'react-native-screens/enableScreens';
+import 'react-native-screens';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
